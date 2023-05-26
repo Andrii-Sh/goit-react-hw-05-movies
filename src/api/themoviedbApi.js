108c03c7a374464d327292afc12a5844f251ed29
Andrii-Sh <114801650+Andrii-Sh@users.noreply.github.com>
2023-05-26 17:49:52 +0300

    added fetch to movie details

--- src/api/themoviedbApi.js
+++ src/api/themoviedbApi.js
@@ -15,3 +15,21 @@ export async function getTrandingMovies() {
     // console.log(response.data.results);
 }
 
+export async function getMovieDetails(id) {
+    const response = await axios.get(`${BASE_URL}3/movie/${id}?language=en-US`, {
+        headers: {
+            Accept: "application/json",
+            Authorization: API_KEY
+        },
+    });
+    return response.data;
+
+}
+
+
+// /movies/get-movie-details
+
+// curl --request GET \
+//      --url 'https://api.themoviedb.org/3/movie/movie_id?language=en-US' \
+//      --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYzAyZDhkMzRkYzcyMjdiZTNjNzA5MzM2N2M4OGViZiIsInN1YiI6IjY0NzA5N2YwNzcwNzAwMDBhOTQ3ZGQ2NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.f3KkCJqrVoPiUwfFpJk-pqYlEvN4ebC6X_KzPPDzK-s' \
+//      --header 'accept: application/json'
--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,11 +1,36 @@
+import React, { useState, useEffect } from 'react';
 import { Outlet, Link, useParams } from 'react-router-dom';
+import { getMovieDetails } from '../api/themoviedbApi';
 
 const MovieDetails = () => {
+  const [movieDetails, setMovieDetails] = useState({});
+
   const { movieId } = useParams();
-  console.log(movieId);
+  // console.log(movieId);
+
+  useEffect(() => {
+    async function fetchMovie() {
+      try {
+        const data = await getMovieDetails(movieId);
+        console.log(data);
+        setMovieDetails(data);
+        console.log('dfgdfgd');
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchMovie();
+  }, [movieId]);
+
   return (
     <div>
-      <div>MovieDetails</div>
+      <div>
+        <img
+          src={`https://image.tmdb.org/t/p/w500/${movieDetails.poster_path}`}
+          alt=""
+        />
+      </div>
       <ul>
         Additional information
         <li>
