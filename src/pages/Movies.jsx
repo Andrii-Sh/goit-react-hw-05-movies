b5eb8ab3ebfb129e6d8baeb634e5dab7277f7cbc
Andrii-Sh <114801650+Andrii-Sh@users.noreply.github.com>
2023-05-28 22:02:03 +0300

    added movies

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,11 +1,28 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { searchMovies } from '../api/themoviedbApi';
 
 const Movies = () => {
   const [query, setQuery] = useState('');
+  const [searchedMovies, setSearchedMovies] = useState([]);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    if (!query) {
+      return;
+    }
+
+    async function fetchMovie() {
+      try {
+        const data = await searchMovies(query);
+        // console.log(data);
+        setSearchedMovies(data.results);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchMovie();
+  }, [query]);
 
   // function handleSubmitForm(evt) {
   //   evt.preventDefault();
@@ -13,18 +30,36 @@ const Movies = () => {
 
   // }
 
-  function handleChangeInput(evt) {
-    setQuery(evt.target.value);
+  // function handleChangeInput(evt) {
+  //   setQuery(evt.target.value);
+  // }
+
+  function handleSubmitForm(evt) {
+    evt.preventDefault();
+    setQuery(evt.target.elements.inputText.value);
+    evt.target.reset();
+    // setQuery('');
   }
 
   return (
     <div>
-      <form>
-        <input type="text" value={query} onChange={handleChangeInput} />
+      <form onSubmit={handleSubmitForm}>
+        <input type="text" name="inputText" />
         <button type="submit"></button>
       </form>
+      <ul>
+        {searchedMovies &&
+          searchedMovies.map(item => (
+            <li key={item.id}>
+              <Link to={`${item.id}`}>{item.title}</Link>
+            </li>
+          ))}
+      </ul>
     </div>
   );
 };
 
 export default Movies;
+// value = { query };
+
+// onChange = { handleChangeInput };
