22789582270e01e4b2162cee665f29eefc260654
Andrii-Sh <114801650+Andrii-Sh@users.noreply.github.com>
2023-05-28 15:21:49 +0300

    started searchForm

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,5 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { searchMovies } from '../api/themoviedbApi';
+
 const Movies = () => {
-  return <div>Movies</div>;
+  const [query, setQuery] = useState('');
+
+  useEffect(() => {}, []);
+
+  // function handleSubmitForm(evt) {
+  //   evt.preventDefault();
+  //   setQuery(evt.target.value);
+
+  // }
+
+  function handleChangeInput(evt) {
+    setQuery(evt.target.value);
+  }
+
+  return (
+    <div>
+      <form>
+        <input type="text" value={query} onChange={handleChangeInput} />
+        <button type="submit"></button>
+      </form>
+    </div>
+  );
 };
 
 export default Movies;
