7321412dbd796e768824a8af71a3788a2cc6b106
Andrii-Sh <114801650+Andrii-Sh@users.noreply.github.com>
2023-05-30 12:22:48 +0300

    added Navigate to Home page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { lazy } from 'react';
 import Layout from '../Layout/Layout';
 
@@ -21,6 +21,7 @@ export const App = () => {
             <Route path="cast" element={<Cast />}></Route>
             <Route path="reviews" element={<Reviews />}></Route>
           </Route>
+          <Route path="*" element={<Navigate to="/" replace={true} />}></Route>
         </Route>
       </Routes>
     </div>
